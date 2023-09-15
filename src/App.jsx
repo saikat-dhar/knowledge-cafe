00b5af23a3f19f23cb230c3bd788d2ec387db8e7
saikat-dhar <saikat01888@gmail.com>
2023-09-15 12:03:56 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+
+      <h1 className='text-5xl text-center mt-8'>knowladge kafe</h1>
+      
+    </>
+  )
+}
+
+export default App
